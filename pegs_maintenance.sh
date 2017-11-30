f228978bc13abbaa244627992bcb9ac203eaf657
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-11-30 11:09:32 +0100

    improved output formatting and logfile name

--- pegs_maintenance.sh
+++ pegs_maintenance.sh
@@ -12,53 +12,53 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 ###TODO### Create auto update script
-_now=$(date +"%Y%m%d.%H%M%S%3N")
+_now=$(date +"%Y-%m-%d_%H.%M.%S")
 PEGS_LOGFILE="/var/log/pegsMaintenance_$_now.log"
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-1/8 ###### Updating apt cache #####################"
+_logline="########## $_timestamp-1/8 ###### Updating apt cache ###########################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 apt-get -qqy update >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-2/8 ###### Updating installed packages ############"
+_logline="########## $_timestamp-2/8 ###### Updating installed packages ##################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 apt-get -qqy --allow-unauthenticated upgrade >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-3/8 ###### Cleaning up obsolete packages ######################"
+_logline="########## $_timestamp-3/8 ###### Cleaning up obsolete packages ################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 apt-get -qqy autoremove >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-4/8 ###### Purging apt package cache ##########################"
+_logline="########## $_timestamp-4/8 ###### Purging apt package cache ####################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 apt-get -qqy clean >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-5/8 ###### Emptying the trash #################################"
+_logline="########## $_timestamp-5/8 ###### Emptying the trash ###########################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 trash-empty >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-6/8 ###### Clearing user cache ################################"
+_logline="########## $_timestamp-6/8 ###### Clearing user cache ##########################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 find /home/* -type f \( -name '*.tmp' -o -name '*.temp' -o -name '*.swp' -o -name '*~' -o -name '*.bak' -o -name '..netrwhist' \) -delete >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-7/8 ###### Deleting old logs ############################"
+_logline="########## $_timestamp-7/8 ###### Deleting old logs ############################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 find /var/log -name "*.log" -mtime +30 -a ! -name "SQLUpdate.log" -a ! -name "updated_days*" -a ! -name "qadirectsvcd*" -exec rm -f {} \;  >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="###### $_timestamp-8/8 ##### Purging TMP directories ##################"
+_logline="########## $_timestamp-8/8 ##### Purging TMP directories #######################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
 # CRUNCHIFY_TMP_DIRS - List of directories to search
@@ -76,6 +76,6 @@ find $CRUNCHIFY_TMP_DIRS -depth -type s -a -ctime $DEFAULT_SOCK_AGE -a -size 0 -
 find $CRUNCHIFY_TMP_DIRS -depth -mindepth 1 -type d -a -empty -a ! -name 'lost+found' -print -delete >> $PEGS_LOGFILE 2>&1
 
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="$_timestamp-###### Maintenance complete ###############################"
+_logline="########## $_timestamp ###### Maintenance complete #############################"
 echo $_logline 2>&1
 echo $_logline >> $PEGS_LOGFILE 2>&1
