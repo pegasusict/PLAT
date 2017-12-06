a24f568b2d38e8d6c3b63d01d7e8ca06e3e2cc4b
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-06 10:21:16 +0100

    wip

--- pegs_postinstall_srv.sh
+++ pegs_postinstall_srv.sh
@@ -1,100 +1,12 @@
 #!/bin/bash
-################################################################################
-## Pegasus' Linux Administration Tools                             VER0.6BETA ##
-## (C)2017 Mattijs Snepvangers                          pegasus.ict@gmail.com ##
-## pegs_postinstall_srv.sh    postinstall script server edition    VER0.6BETA ##
-## License: GPL v3                         Please keep my name in the credits ##
-################################################################################
-
-# Make sure only root can run this script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-_now=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
-PEGS_LOGFILE="/var/log/pegsPostInstall_$_now.log"
-echo "################################################################################\n" 2>&1 | tee -a $PEGS_LOGFILE
-echo "## Pegasus' Linux Administration Tools - Post Install Script         V0.6Beta ##\n" 2>&1 | tee -a $PEGS_LOGFILE
-echo "## (c) 2017 Mattijs Snepvangers                         pegasus.ict@gmail.com ##\n" 2>&1 | tee -a $PEGS_LOGFILE
-echo "################################################################################\n" 2>&1 | tee -a $PEGS_LOGFILE
-echo "\n" 2>&1 | tee -a $PEGS_LOGFILE
-
-printfx() {
-
-    TEMP=`getopt -o hbrkwels:c:v:f:p: --long help,write,keep,reset,bold,format:,script:,color:,print: -n "$FUNCNAME" -- "$@"`
-
-    if [ $? != 0 ] ; then return 1 ; fi
-
-    eval set -- "$TEMP";
-
-    local format='%s\n' escape='-E' line='-n' script clear='tput sgr0';
-
-    while [[ ${1:0:1} == - ]]; do
-        [[ $1 =~ ^-h|--help ]] && {
-            cat <<-EOF
-            USAGE: $FUNCNAME [OPTIONS] [TEXT]
-
-            OPTIONS
-
-              -s  Adds a line of text to tput script
-              -c  Adds a line of text to tput script that sets the foreground
-                  color
-              -b  Adds a line of text to tput script that activates emboldened
-                  text
-              -f  Sets the final printf format
-              -w  Forces an immediate write of tput script
-              -p  Immediately prints data to standard out
-              -e  Enabled backslash interpretation in immediate print operations
-              -l  Enables line output in immediate print operations
-              -k  disables terminal reset on return
-              -r  Forces an immediate reset of terminal settings
-              -v  Adds multiple lines of script from a shell variable label
-
-              --script  Same as -s
-              --color   Same as -c
-              --bold    Same as -b
-              --format  Same as -f
-              --write   Same as -w
-              --print   Same as -p
-              --keep    Same as -k
-              --reset   Same as -r
-
-            EOF
-            return;
-        };
-
-        [[ $1 == -- ]] && { shift; break; };
-        [[ $1 == -l ]] && { line=''; shift 1; continue; };
-        [[ $1 =~ ^-s|--script$ ]] && { script+="$2"$'\n'; shift 2; continue; };
-        [[ $1 =~ ^-c|--color$ ]] && { script+="setf $2"$'\n'; shift 2; continue; };
-        [[ $1 =~ ^-b|--bold$ ]] && { script+="bold"$'\n'; shift 1; continue; };
-        [[ $1 =~ ^-f|--format$ ]] && { format="${2}"; shift 2; continue; };
-        [[ $1 =~ ^-w|--write$ ]] && { tput -S <<<"$script"; script=''; shift 1; continue; };
-        [[ $1 =~ ^-p|--print$ ]] && { echo $escape $line "${2}"; escape='-E' line='-n'; shift 2; continue; };
-        [[ $1 =~ ^-k|--keep$ ]] && { clear='true'; shift 1; continue; };
-        [[ $1 =~ ^-r|--reset$ ]] && { tput sgr0; shift 1; continue; };
-        [[ $1 =~ ^-v ]] && { script+="${!2}"$'\n'; shift 2; continue; };
-        [[ $1 =~ ^-e ]] && { escape=$1; shift 1; continue; };
-
-        break;
-    done
-
-    echo $format;
-    tput -S <<<"$script";
-    (( $# )) && printf "$format" "$@";
-    $clear;
-
-}
-
 
 # Install extra ppa's
 _timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
 _logline="$_timestamp-1/7 ###### installing extra PPA's #############################"
 echo $_logline 2>&1 | tee -a $PEGS_LOGFILE
 echo 'deb http://archive.ubuntu.com/ubuntu main restricted universe multiverse proposed backports' >> /tmp/pegsaddition.list
-sudo cp /tmp/pegsaddition.list /etc/apt/sources.list.d/
-rm /tmp/pegsaddition.list
+mv /tmp/pegsaddition.list /etc/apt/sources.list.d/
+
 #add-apt-repository -y ppa:juju/stable >>"$PEGS_LOGFILE" 2>&1
 #add-apt-repository -y ppa:landscape/17.03 >>"$PEGS_LOGFILE" 2>&1
 
