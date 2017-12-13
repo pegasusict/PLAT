9613bca2b733cd340d926b4262b58d1f98d64bd2
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 10:54:21 +0100

    test

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 active_containers="$(lxc list -c ns | grep -i running)"
+echo $active_containers
 containers="(${egrep '\ba-zA-Z{1}a-zA-Z0-9\-{,61}a-zA-Z0-9{1}\b' $active_containers)"
 print "$containers"
