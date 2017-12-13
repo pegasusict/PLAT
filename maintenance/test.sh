c63b5d634c87f7c0e345def602fc9cafda74e60b
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 21:59:45 +0100

    test

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -12,23 +12,8 @@ _logline="########## $_timestamp-1/10 ###### Scanning for containers ###########
 echo $_logline 2>&1 | tee -a $PLAT_LOGFILE
 active_containers=$(lxc list -c ns | grep -i running)
 inactive_containers=$(lxc list -c ns | grep -i stopped)
-active_containers=$(echo "$active_containers" | grep -Po "\b[a-zA-Z0-9][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| RUNNING)")
-inactive_containers=$(echo $"inactive_containers" | grep -Po "\b[a-zA-Z0-9][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| STOPPED)")
-################################################################################
-_timestamp=$(date +"%Y-%m-%d_%H.%M.%S,%3N")
-_logline="########## $_timestamp-2/10 ###### Containers found: ###########################"
-echo $_logline 2>&1 | tee -a $PLAT_LOGFILE
-active_containers_found = 0
-inactive_containers_found = 0
-for (( i=0; i<${#active_containers[@]}; i++ ))
-do
-    echo "Active Container $i: ${active_containers[$i]}"
-    active_containers_found++
-done
-echo "found $active_containers_found active containers."
-for (( i=0; i<${#inactive_containers[@]}; i++ ))
-do
-    echo "Inactive Container $i: ${inactive_containers[$i]}"
-    inactive_containers_found++
-done
-echo "found $inactive_containers_found inactive containers."
+active_containers=$(echo "$active_containers" | grep -Po "\b[a-zA-Z][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| RUNNING)")
+inactive_containers=$(echo "$inactive_containers" | grep -Po "\b[a-zA-Z][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| STOPPED)")
+
+echo "test result:"
+echo "$active_containers"
