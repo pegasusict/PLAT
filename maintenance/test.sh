ab20608ecad8d75de6e616261313560e872548a3
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 11:14:53 +0100

    test

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -5,4 +5,4 @@ findcontainers='\ba-zA-Z\{1\}a-zA-Z0-9\-\{,61\}a-zA-Z0-9\{1\}\b'
 if [[ $active_containers =~ $findcontainers ]]; then
     $containers=${BASH_REMATCH[1]}
 fi
-print "$containers"
+print $containers
