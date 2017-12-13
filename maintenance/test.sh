bbb45d3a884fda390cc85ace187d04145c8aea58
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 10:47:15 +0100

    WIP

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-"$(lxc list -c ns | grep -i running)" = containers
-containers = "(${egrep '\ba-zA-Z{1}a-zA-Z0-9\-{,61}a-zA-Z0-9{1}\b' $active_containers)"
+active_containers="$(lxc list -c ns | grep -i running)"
+containers="(${egrep '\ba-zA-Z{1}a-zA-Z0-9\-{,61}a-zA-Z0-9{1}\b' $active_containers)"
 print "$containers"
