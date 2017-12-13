af4111f6e2204ed50b7900cd0b26339f61380c91
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 11:09:43 +0100

    test

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 active_containers="$(lxc list -c ns | grep -i running)"
 echo $active_containers
-containers="(${egrep '\ba-zA-Z{1}a-zA-Z0-9\-{,61}a-zA-Z0-9{1}\b' $active_containers)"
+findcontainers='\ba-zA-Z{1}a-zA-Z0-9\-{,61}a-zA-Z0-9{1}\b'
+if [[ $active_containers =~ $findcontainers ]]; then
+    $containers=${BASH_REMATCH[1]}
+fi
 print "$containers"
