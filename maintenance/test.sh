e2664c0196940b9c6e4119434380c978799ca7ab
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 18:06:33 +0100

    Update test.sh
    According to stackoverflow answer by Nahuel Fouilleul

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 active_containers="$(lxc list -c ns | grep -i running)"
 echo $active_containers
-findcontainers='\b(a-zA-Za-zA-Z0-9\-{,61}a-zA-Z0-9)\b'
-echo $active_containers | sed -e "$findcontainers"
+#findcontainers='\b(a-zA-Za-zA-Z0-9\-{,61}a-zA-Z0-9)\b'
+#echo $active_containers | sed -e "$findcontainers"
+
+echo "$active_containers" | grep -Po "\b[a-zA-Z0-9][-a-zA-Z0-9]{1,61}[a-zA-Z0-9](?=\s*\| RUNNING)"
