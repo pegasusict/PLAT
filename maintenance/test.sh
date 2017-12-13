ce3320488d5c9c1faa3de60cba1ec0a7ac623472
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-13 13:26:10 +0100

    test

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -2,4 +2,4 @@
 active_containers="$(lxc list -c ns | grep -i running)"
 echo $active_containers
 findcontainers='\b(a-zA-Za-zA-Z0-9\-{,61}a-zA-Z0-9)\b'
-echo $active_containers | sed -e "$findcontainers"
+echo $active_containers | sed -e $findcontainers
