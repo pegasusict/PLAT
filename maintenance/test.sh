cb86ed9e207078965d7e68050ebcbb034d66dd45
Mattijs Snepvangers <pegasus.ict@gmail.com>
2017-12-14 10:30:14 +0100

    testing...

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -7,13 +7,28 @@ active_containers=$(lxc list -c ns | grep -i running)
 inactive_containers=$(lxc list -c ns | grep -i stopped)
 active_containers=$(echo "$active_containers" | grep -Po "\b[a-zA-Z][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| RUNNING)")
 inactive_containers=$(echo "$inactive_containers" | grep -Po "\b[a-zA-Z][-a-zA-Z]{0,61}[a-zA-Z0-9](?=\s*\| STOPPED)")
-
-echo "test result:"
-echo "$active_containers"
-#####################################################################
-IFS=$'\n' activecontainers=($active_containers)
-#active_containers=readarray -t y <<<"$active_containers"
-echo ${activecontainers[0]}
-echo "====================="
-echo ${activecontainers[1]}
-echo "====================="
+IFS=$'\n'
+activecontainers=($active_containers)
+inactivecontainers=($inactive_containers)
+active_containers_found=${#activecontainers[@]}
+inactive_containers_found=${#inactivecontainers[@]}
+if [active_containers_found > 0];
+then
+   echo "$active_containers_found active containers found:"
+   for (( i=0; i<${active_containers_found}; i++ ));
+   do
+      echo ${activecontainers[$i]}
+   done
+else
+   echo "No active containers found"
+fi
+if [inactive_containers_found > 0];
+then
+   echo "$inactive_containers_found inactive containers found:"
+   for (( i=0; i<${inactive_containers_found}; i++ ));
+   do
+      echo ${inactivecontainers[$i]}
+   done
+else
+   echo "No inactive containers found"
+fi
