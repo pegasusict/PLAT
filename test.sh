5e2f65f520f0f91bfa18e9c29f70551a5434f600
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-05-07 11:10:15 +0200

    implementing unit tester

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -p
+############################################################################
+# Pegasus' Linux Administration Tools #				  Pegasus' PLAT Tester #
+# (C)2017-2018 Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
+# License: GPL v3					  #	Please keep my name in the credits #
+############################################################################
+
+#######################################################
+# PROGRAM_SUITE="Pegasus' Linux Administration Tools" #
+# SCRIPT_TITLE="Function Tester Script"				  #
+# MAINTAINER="Mattijs Snepvangers"					  #
+# MAINTAINER_EMAIL="pegasus.ict@gmail.com"			  #
+# VERSION_MAJOR=0									  #
+# VERSION_MINOR=0									  #
+# VERSION_PATCH=0									  #
+# VERSION_STATE="PRE-ALPHA"							  #
+# VERSION_BUILD=20180507							  #
+#######################################################
+
+
+
+main_menu
+	echo PLAT Functionality Tester
+	echo
+	echo Main Menu
+	echo
+
+
+
+ANSWER=prompt "Which module would you like to test?\nIf you would like to test more than one, please separate the values with a space."
