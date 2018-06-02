ade916a3f90c25df8fb09c609b4cb45690a8cabe
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-02 23:38:19 +0200

    WIP

---
+++ lib/plat_manager-functions.inc.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+################################################################################
+# Pegasus' Linux Administration Tools	#		PLAT Manager Functions Library #
+# (C)2017-2018 Mattijs Snepvangers		#				 pegasus.ict@gmail.com #
+# License: MIT							#	Please keep my name in the credits #
+################################################################################
+
+#########################################################
+# PROGRAM_SUITE="Pegasus' Linux Administration Tools"	#
+# SCRIPT_TITLE="Plat Manager Functions Library"			#
+# MAINTAINER="Mattijs Snepvangers"						#
+# MAINTAINER_EMAIL="pegasus.ict@gmail.com"				#
+# VERSION_MAJOR=0										#
+# VERSION_MINOR=0										#
+# VERSION_PATCH=0										#
+# VERSION_STATE="PRE-ALPHA"								#
+# VERSION_BUILD=20180602								#
+# LICENSE="MIT"											#
+#########################################################
+
+### Basic program ##############################################################
+get_args() {
+	getopt --test > /dev/null
+	if [[ $? -ne 4 ]]
+	then
+		err_line "I’m sorry, \"getopt --test\" failed in this environment."
+		exit 1
+	fi
+	OPTIONS="hv:r:c:g:l:t:S:P:R:"
+	LONG_OPTIONS="help,verbosity:,role:,containertype:garbageage:logage:tmpage:"
+	PARSED=$(getopt -o $OPTIONS --long $LONG_OPTIONS -n "$0" -- "$@")
+	if [ $? -ne 0 ]
+		then usage
+	fi
+	eval set -- "$PARSED"
+	while true; do
+		case "$1" in
+			-h|--help			) usage ; shift ;;
+			-v|--verbosity		) setverbosity $2 ; shift 2 ;;
+			-r|--role			) checkrole $2; shift 2 ;;
+			-c|--containertype	) checkcontainer $2; shift 2 ;;
+			-g|--garbageage		) GABAGE_AGE=$2; shift 2 ;;
+			-l|--logage			) LOG_AGE=$2; shift 2 ;;
+			-t|--tmpage			) TMP_AGE=$2; shift 2 ;;
+			--					) shift; break ;;
+			*					) break ;;
+		esac
+	done
+}
+
+usage() {
+	version
+	cat <<-EOT
+		USAGE: sudo bash $SCRIPT -h
+		        or
+		       sudo bash $SCRIPT -r <SYSTEM_ROLE> [ -c <containertype> ] [ -v INT ] [ -g <garbageage> ] [ -l <logage> ] [ -t <tmpage> ]
+
+		OPTIONS
+
+		   -r or --role tells the script what kind of system we are dealing with.
+		      Valid options: ws, poseidon, mainserver, container << REQUIRED >>
+		   -c or --containertype tells the script what kind of container we are working on.
+		      Valid options are: basic, nas, web, x11, pxe, router << REQUIRED if -r=container >>
+		   -v or --verbosity defines the amount of chatter. 0=CRITICAL, 1=WARNING, 2=INFO, 3=VERBOSE, 4=DEBUG. default=2
+		   -g or --garbageage defines the age (in days) of garbage (trashbins & temp files) being cleaned, default=7
+		   -l or --logage defines the age (in days) of logs to be purged, default=30
+		   -t or --tmpage define how long temp files should be untouched before they are deleted, default=2
+		   -h or --help prints this message
+
+		  The options can be used in any order
+		EOT
+	exit 3
+}
--- postinstall.sh
+++ postinstall.sh
@@ -24,7 +24,7 @@ init() {
 	declare -gr VERSION_MINOR=4
 	declare -gr VERSION_PATCH=28
 	declare -gr VERSION_STATE="PRE-ALPHA"
-	declare -gr VERSION_BUILD=20180525
+	declare -gr VERSION_BUILD=20180602
 	declare -gr LICENSE="MIT"
 	###############################################################################
 	declare -gr PROGRAM="$PROGRAM_SUITE - $SCRIPT_TITLE"
@@ -33,14 +33,13 @@ init() {
 }
 
 prep() {
-	import "BASH_FUNC_LIB/default.inc.bash"
+	import "PBFL/default.inc.bash"
 	create_dir "$LOG_DIR"
 	import "lib/postinstall-functions.inc.bash"
 	header
 	goto_base_dir
 	parse_ini $INI_FILE
 	get_args $@
-	create_dir $TARGET_SCRIPT_DIR
 }
 
 import() {
@@ -55,48 +54,44 @@ import() {
 }
 
 main() {
-	# check whether systemrole_container has been checked and if yes,
+	# check whether SYSTEM_ROLE_container has been checked and if yes,
 	#+ nas,web,ws,pxe,basic or router have been checked
-	if [[ $SYSTEMROLE_CONTAINER == true ]]
+	create_dir $TARGET_SCRIPT_DIR
+	if [[ $SYSTEM_ROLE[CONTAINER] == true ]]
 	then
-		dbg_line "SYSTEMROLE_CONTAINER was chosen, see if there's a containerrole as well"
-		if [[ $SYSTEMROLE_NAS == true ]] || [[ $SYSTEMROLE_WEB == true ]] || [[ $SYSTEMROLE_WS == true ]] || [[ $SYSTEMROLE_PXE == true ]] || [[ $SYSTEMROLE_BASIC == true ]] || [[ $SYSTEMROLE_ROUTER == true ]]
+		dbg_line "SYSTEM_ROLE CONTAINER was chosen, see if there's a containerrole as well"
+		if [[ $SYSTEM_ROLE[BASIC] == true ]] || [[ $SYSTEM_ROLE[WS] == true ]] || [[ $SYSTEM_ROLE[SERVER] == true ]] || [[ $SYSTEM_ROLE[NAS] == true ]] || [[ $SYSTEM_ROLE[PXE] == true ]] || [[ $SYSTEM_ROLE[ROUTER] == true ]] || [[ $SYSTEM_ROLE[WEB] == true ]] || [[ $SYSTEM_ROLE[X11] == true ]]
 			then
 				dbg_line "a CONTAINER_ROLE has been chosen; we're good :-) "
 			else
-				crit_line "No container role has been designated"
-				exit 1
+				exit 1 "No container role has been designated"
 		fi
 	fi
 	################################################################################
-	if [[ $SYSTEMROLE_MAINSERVER == true ]]
+	if [[ $SYSTEM_ROLE[MAINSERVER] == true ]]
 	then
 		info_line "Injecting interfaces file into network config"
 		cat lxchost_interfaces.txt > /etc/network/interfaces
 	fi
 	################################################################################
+	info_line "Copying Ubuntu sources and some extras"
+	cp apt/base.list /etc/apt/sources.list.d/ 2>&1 | err_line
+	################################################################################
 	info_line "Installing extra PPA's"
-	verb_line "Copying Ubuntu sources and some extras"	;	cp apt/base.list /etc/apt/sources.list.d/ 2>&1 | err_line
-	verb_line "Adding GetDeb PPA key"			;	add_ppa "wget" "http://archive.getdeb.net/getdeb-archive.key"
-	verb_line "Adding VirtualBox PPA key"		;	add_ppa "wget" "http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc"
-	verb_line "Adding Webmin PPA key"			;	add_ppa "wget" "http://www.webmin.com/jcameron-key.asc"
-	verb_line "Adding WebUpd8 PPA key"			;	add_ppa "apt-key" "keyserver.ubuntu.com" "4C9D234C"
-	if [[ $SYSTEMROLE_WS == true ]]
-	then
-		verb_line "Adding FreeCad PPA"			;	add_ppa "aar" "ppa:freecad-maintainers/freecad-stable"
-		verb_line "Adding GIMP PPA key"			;	add_ppa "apt-key" "keyserver.ubuntu.com" "614C4B38"
-		verb_line "Adding Gnome3 Extras PPA"	;	add_ppa "apt-key" "keyserver.ubuntu.com" "3B1510FD"
-		verb_line "Adding Google Chrome PPA"	;	add_ppa "wget" "https://dl.google.com/linux/linux_signing_key.pub"
-		verb_line "Adding Highly Explosive PPA"	;	add_ppa "apt-key" "keyserver.ubuntu.com" "93330B78"
-		verb_line "Adding MKVToolnix PPA"		;	add_ppa "wget" "http://www.bunkus.org/gpg-pub-moritzbunkus.txt"
-		verb_line "Adding Opera (Beta) PPA"		;	add_ppa "wget" "http://deb.opera.com/archive.key"
-		verb_line "Adding OwnCloud Desktop PPA"	;	add_ppa "wget" "http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_16.04/Release.key"
-		verb_line "Adding Wine PPA"				;	add_ppa "apt-key" "keyserver.ubuntu.com" "883E8688397576B6C509DF495A9A06AEF9CB8DB0"
-	fi
-	if	[[ $SYSTEMROLE_NAS == true ]]
-	then
-		verb_line "Adding Syncthing PPA"		;	add_ppa "wget" "https://syncthing.net/release-key.txt"
-	fi
+	for ROLE in $SYSTEM_ROLE
+	do
+		if [[ "$ROLE"==true ]]
+		then
+			for PPA_KEY in PPA_KEYS
+			do
+
+				info_line "Adding $PPA_KEY PPA key"
+				echo "add_ppa" "$PPA_KEYS[PPA_KEY][0]" "$PPA_KEYS[PPA_KEY][1]" "$PPA_KEYS[PPA_KEY][2]"
+				add_ppa_key "$PPA_KEYS[PPA_KEY][0]" "$PPA_KEYS[PPA_KEY][1]" "$PPA_KEYS[PPA_KEY][2]"
+			done
+		fi
+	done
+
 	################################################################################
 	info_line "removing duplicate lines from source lists"
 	perl -i -ne 'print if ! $a{$_}++' "/etc/apt/sources.list /etc/apt/sources.list.d/*" 2>&1 | dbg_line
@@ -105,16 +100,16 @@ main() {
 	info_line "Installing updates"
 	apt-get --allow-unauthenticated upgrade -qy 2>&1 | dbg_line
 	######
-	info_line "Installing extra packages"	;	apt-inst mc trash-cli snapd git
-	if [[ $SYSTEMROLE_WS == true ]]			;	then apt-inst synaptic tilda audacious samba wine-stable playonlinux winetricks; fi
-	if [[ $SYSTEMROLE_POSEIDON == true ]]	;	then apt-inst picard audacity calibre fastboot adb fslint gadmin-proftpd geany* gprename lame masscan forensics-all forensics-extra forensics-extra-gui forensics-full chromium-browser gparted ; fi
-	if [[ $SYSTEMROLE_WEB == true ]]		;	then apt-inst apache2 phpmyadmin mysql-server mytop proftpd webmin ; fi
-	if [[ $SYSTEMROLE_NAS == true ]]		;	then apt-inst samba nfsd proftpd ; fi
-	if [[ $SYSTEMROLE_PXE == true ]]		;	then apt-inst atftpd ; fi
-	if [[ $SYSTEMROLE_LXCHOST == true ]]	;	then apt-inst python3-crontab lxc lxcfs lxd lxd-tools bridge-utils xfsutils-linux criu apt-cacher-ng; fi
-	if [[ $SYSTEMROLE_SERVER == true ]]		;	then apt-inst ssh-server screen webmin; fi
-	if [[ $SYSTEMROLE_BASIC == true ]]		;	then echo "" ; fi
-	if [[ $SYSTEMROLE_ROUTER == true ]]		;	then apt-inst bridge-utils ufw; fi
+	info_line "Installing extra packages"
+	###TODO### REWRITE TO INCORPORATE ARRAY
+	if [[ $SYSTEM_ROLE_POSEIDON == true ]]	;	then apt-inst audacity calibre fastboot adb fslint gadmin-proftpd geany* gprename lame masscan forensics-all forensics-extra forensics-extra-gui forensics-full gparted picard ; fi
+	if [[ $SYSTEM_ROLE_WEB == true ]]		;	then apt-inst apache2 phpmyadmin mysql-server mytop proftpd webmin ; fi
+	if [[ $SYSTEM_ROLE_NAS == true ]]		;	then apt-inst samba nfsd proftpd ; fi
+	if [[ $SYSTEM_ROLE_PXE == true ]]		;	then apt-inst atftpd ; fi
+	if [[ $SYSTEM_ROLE_LXCHOST == true ]]	;	then apt-inst python3-crontab lxc lxcfs lxd lxd-tools bridge-utils xfsutils-linux criu apt-cacher-ng; fi
+	if [[ $SYSTEM_ROLE_SERVER == true ]]		;	then apt-inst ssh-server screen webmin; fi
+	if [[ $SYSTEM_ROLE_BASIC == true ]]		;	then echo "" ; fi
+	if [[ $SYSTEM_ROLE_ROUTER == true ]]		;	then apt-inst bridge-utils ufw; fi
 	################################################################################
 	info_line "Installing extra software"
 	verb_line "Installing TeamViewer"
@@ -124,7 +119,7 @@ main() {
 	download "https://download.teamviewer.com/download/teamviewer_i386.deb"
 	install teamviewer_i386.deb
 	apt-get install -fy 2>&1 | verb_line
-	if [[ $SYSTEMROLE_POSEIDON == true ]]
+	if [[ $SYSTEM_ROLE_POSEIDON == true ]]
 	then
 		verb_line "Installing StarUML"
 		download "http://nl.archive.ubuntu.com/ubuntu/pool/main/libg/libgcrypt11/libgcrypt11_1.5.3-2ubuntu4.5_amd64.deb"
@@ -140,17 +135,17 @@ main() {
 	####
 	info_line "Building maintenance script"
 	build_maintenance_script "$MAINTENANCE_SCRIPT$MAINTENANCE_SCRIPT"
-	if [[ $SYSTEMROLE_LXCHOST == true ]]
+	if [[ $SYSTEM_ROLE_LXCHOST == true ]]
 	then
 		build_maintenance_script "$MAINTENANCE_SCRIPT$CONTAINER_SCRIPT"
 	fi
 	cp "$LIB_DIR$LIB" "$TARGET_SCRIPT_DIR$LIB_DIR"
 	####
-	if [[ $SYSTEMROLE_CONTAINER == true ]]
+	if [[ $SYSTEM_ROLE_CONTAINER == true ]]
 	then dbg_line "NOT adding $MAINTENANCE_SCRIPT to sheduler"
 	else
 		verb_line "adding $MAINTENANCE_SCRIPT to sheduler"
-		if [[ $SYSTEMROLE_MAINSERVER == true ]]
+		if [[ $SYSTEM_ROLE_MAINSERVER == true ]]
 		then
 			CRON_FILE="/etc/crontab"
 			LINE_TO_ADD="\n0 6 * * 0 root bash $TARGET_SCRIPT_DIR$MAINTENANCE_SCRIPT #PLAT maintenance"
