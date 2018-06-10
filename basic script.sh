4dddb591ffda1c8e8efe9470a785334a908cb7e1
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-10 08:09:29 +0200

    created basic script template and gathered  research material on OOP with Bash
    .
    
    [Ticket: X]

---
+++ basic script.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+############################################################################
+# Pegasus' Linux Administration Tools #					<<script title>> #
+# (C)2017-<<year>> Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
+# License: MIT						  # Please keep my name in the credits #
+############################################################################
+
+# !!! first replace (ctrl-h)
+# <<year>> with the current year
+# "<<date>>" with todays date
+# <<script title>> with the title of the script and adjust the number of tabs if needed for proper alignment
+
+START_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
+# Making sure this script is run by bash to prevent mishaps
+if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then bash "$0" "$@" ; exit "$?" ; fi
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" ; exit 1 ; fi
+echo "$START_TIME ## Starting PostInstall Process #######################"
+### FUNCTIONS ###
+
+init() {
+	################### PROGRAM INFO ##############################################
+	declare -gr PROGRAM_SUITE="Pegasus' Linux Administration Tools"
+	declare -gr SCRIPT="${0##*/}"
+	declare -gr SCRIPT_DIR="${0%/*}"
+	declare -gr SCRIPT_TITLE="<<script title>>"
+	declare -gr MAINTAINER="Mattijs Snepvangers"
+	declare -gr MAINTAINER_EMAIL="pegasus.ict@gmail.com"
+	declare -gr COPYRIGHT="(c)2017-$(date +"%Y")"
+	declare -gr VERSION_MAJOR=0
+	declare -gr VERSION_MINOR=0
+	declare -gr VERSION_PATCH=0
+	declare -gr VERSION_STATE="PRE-ALPHA"
+	declare -gr VERSION_BUILD="<<date>>"
+	declare -gr LICENSE="MIT"
+	###############################################################################
+	declare -gr PROGRAM="$PROGRAM_SUITE - $SCRIPT_TITLE"
+	declare -gr SHORT_VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-$VERSION_STATE"
+	declare -gr VERSION="Ver$SHORT_VERSION build $VERSION_BUILD"
+}
+
+prep() {
+	import "PBFL/default.inc.bash"
+	create_dir "$LOG_DIR"
+	import "lib/postinstall-functions.inc.bash"
+	header
+	goto_base_dir
+	parse_ini $INI_FILE
+	get_args $@
+}
+
+import() {
+	local _FILE="$1"
+	if [[ -f "$_FILE" ]]
+	then
+		source "$_FILE"
+	else
+		crit_line "File $_FILE not found!"
+		exit 1
+	fi
+}
+
+main() {
+
+
+
+
+
+}
+
+###########
+init
+prep $@
+
+main
