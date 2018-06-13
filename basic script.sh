0537a58a760b2aba070de25c53cc0fed5302c218
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-13 11:46:03 +0200

    bugfixes, licencing and  default values updated

--- basic script.sh
+++ basic script.sh
@@ -9,6 +9,18 @@
 # <<year>> with the current year
 # "<<date>>" with todays date
 # <<script title>> with the title of the script and adjust the number of tabs if needed for proper alignment
+# DEBUG SWITCH
+
+set -o xtrace	# Trace the execution of the script
+
+# to prevent mishaps when using cd with relative paths
+unset CDPATH
+
+# A better class of script...
+set -o errexit	# Exit on most errors (see the manual)
+set -o errtrace	# Make sure any error trap is inherited
+set -o nounset	# Disallow expansion of unset variables
+set -o pipefail	# Use last non-zero exit code in a pipeline
 
 START_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
 # Making sure this script is run by bash to prevent mishaps
@@ -19,7 +31,7 @@ echo "$START_TIME ## Starting PostInstall Process #######################"
 ### FUNCTIONS ###
 
 init() {
-	################### PROGRAM INFO ##############################################
+	################### PROGRAM INFO ###########################################
 	declare -gr PROGRAM_SUITE="Pegasus' Linux Administration Tools"
 	declare -gr SCRIPT="${0##*/}"
 	declare -gr SCRIPT_DIR="${0%/*}"
@@ -33,7 +45,7 @@ init() {
 	declare -gr VERSION_STATE="PRE-ALPHA"
 	declare -gr VERSION_BUILD="<<date>>"
 	declare -gr LICENSE="MIT"
-	###############################################################################
+	############################################################################
 	declare -gr PROGRAM="$PROGRAM_SUITE - $SCRIPT_TITLE"
 	declare -gr SHORT_VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH-$VERSION_STATE"
 	declare -gr VERSION="Ver$SHORT_VERSION build $VERSION_BUILD"
@@ -68,7 +80,7 @@ main() {
 
 }
 
-###########
+################################################################################
 init
 prep $@
 
