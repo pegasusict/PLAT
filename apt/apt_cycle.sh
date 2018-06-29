4268e99bd98a4040c128a25c3cbebc05e26bee22
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-29 10:02:37 +0200

    wip

--- apt/apt_cycle.sh
+++ apt/apt_cycle.sh
@@ -12,9 +12,10 @@ if [ "$(ps -p "$$" -o comm=)" != "bash" ]; then bash "$0" "$@" ; exit "$?" ; fi
 # Make sure only root can run this script
 if [[ $EUID -ne 0 ]]; then echo "This script must be run as root" ; exit 1 ; fi
 echo "$START_TIME ## Starting Update Process #######################"
-echo "Updating apt cache"                  ;	apt-get -qqy update
-echo "Updating installed packages"         ;	apt-get -qqy --allow-unauthenticated upgrade
-echo "Cleaning up obsolete packages"       ;	apt-get -qqy auto-remove --purge
-echo "Clearing old/obsolete package cache" ;	apt-get -qqy autoclean
+echo "Updating apt cache"					;	apt-get -qqy update
+echo "checking for distribution upgrade"	;	apt-get -qqy distupgrade
+echo "Updating installed packages"			;	apt-get -qqy --allow-unauthenticated upgrade
+echo "Cleaning up obsolete packages"		;	apt-get -qqy auto-remove --purge
+echo "Clearing old/obsolete package cache"	;	apt-get -qqy autoclean
 END_TIME=$(date +"%Y-%m-%d_%H.%M.%S.%3N")
 echo "$END_TIME ## Update Process Finished ########################"
