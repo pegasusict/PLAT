9d3fd047a7372bfa0aa0a43a5e846c92759b925b
Mattijs Snepvangers <30695552+pegasusict@users.noreply.github.com>
2021-10-21 10:13:34 +0200

    Update install_docker-ce.sh

--- install_docker-ce.sh
+++ install_docker-ce.sh
@@ -1,9 +1,40 @@
-#!/bin/sh
-apt-get -qqy update && apt-get -qy dist-upgrade && apt-get -qy install screen mc curl && apt-get -qy autoremove && apt-get -qy autoclean
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get -qqy update && apt-get -qy dist-upgrade && apt-get -qy install docker-ce
+#!/usr/bin/env bash
+clear
+apt-get update; apt-get install -qy apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-docker volume create portainer_data
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update; apt-get install docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker $USER
+newgrp docker
+
+docker version
+pause
+########################################################################################################################
+clear
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | \
+grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+chmod +x docker-compose-linux-x86_64
+mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+
+docker-compose version
+########################################
+pause
+clear
+
+curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose \
+-o /etc/bash_completion.d/docker-compose
+source /etc/bash_completion.d/docker-compose
+
+########################################################################################################################
+mkdir ~/portainer
+cd ~/portainer
+
+docker pull portainer/portainer
+docker tag portainer/portainer portainer
+export CONT_NAME="portainer"
+
+docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v ~/portainer:/data \
+--name ${CONT_NAME} portainer
